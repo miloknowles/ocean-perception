dc639b433282a758cbe9878ab7ad72912434fec1
Milo Knowles <mknowles@mit.edu>
2021-01-27 07:26:15 -0800

    Update SGBM test

--- test/stereo_matching/sgbm_test.cpp
+++ test/stereo_matching/sgbm_test.cpp
@@ -24,28 +24,32 @@ TEST(StereoMatchingTest, TestSGBM)
   // const Image1b il = cv::imread("./resources/farmsim_01_left.png", CV_LOAD_IMAGE_GRAYSCALE);
   // const Image1b ir = cv::imread("./resources/farmsim_01_right.png", CV_LOAD_IMAGE_GRAYSCALE);
 
-  // const Image1b il = cv::imread("./resources/caddy_32_left.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-  // const Image1b ir = cv::imread("./resources/caddy_32_right.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+  const Image1b il = cv::imread("./resources/caddy_32_left.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+  const Image1b ir = cv::imread("./resources/caddy_32_right.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+  cv::imshow("left image", il);
+  cv::imshow("right image", ir);
 
-  Image3b lraw = cv::imread("./resources/caddy_32_left.jpg", cv::IMREAD_COLOR);
-  Image3b rraw = cv::imread("./resources/caddy_32_right.jpg", cv::IMREAD_COLOR);
-  Image3f Il = CastImage3bTo3f(lraw);
-  Image3f Ir = CastImage3bTo3f(rraw);
+  // Image3b lraw = cv::imread("./resources/caddy_32_left.jpg", cv::IMREAD_COLOR);
+  // Image3b rraw = cv::imread("./resources/caddy_32_right.jpg", cv::IMREAD_COLOR);
+  // Image3f Il = CastImage3bTo3f(lraw);
+  // Image3f Ir = CastImage3bTo3f(rraw);
 
-  Image3f Jl = im::Normalize(im::NormalizeColorIlluminant(Il));
-  Image3f Jr = im::Normalize(im::NormalizeColorIlluminant(Ir));
+  // Image3f Jl = im::Normalize(im::NormalizeColorIlluminant(Il));
+  // Image3f Jr = im::Normalize(im::NormalizeColorIlluminant(Ir));
 
-  Image1f Gl, Gr;
-  cv::cvtColor(Jl, Gl, CV_BGR2GRAY);
-  cv::cvtColor(Jr, Gr, CV_BGR2GRAY);
+  // Image1f Gl, Gr;
+  // cv::cvtColor(Jl, Gl, CV_BGR2GRAY);
+  // cv::cvtColor(Jr, Gr, CV_BGR2GRAY);
 
-  Image1b Gbl, Gbr;
-  Gl.convertTo(Gbl, CV_8UC1, 255.0f);
-  Gr.convertTo(Gbr, CV_8UC1, 255.0f);
+  // Image1b Gbl, Gbr;
+  // Gl.convertTo(Gbl, CV_8UC1, 255.0f);
+  // Gr.convertTo(Gbr, CV_8UC1, 255.0f);
 
-  cv::imshow("Gbl", Gbl);
+  // cv::imshow("Gbl", Gbl);
 
-  const Image1f& disp = sm::EstimateDisparity(Gbl, Gbr);
+  // const Image1f& disp = sm::EstimateDisparity(Gbl, Gbr);
+
+  const Image1f& disp = sm::EstimateDisparity(il, ir);
 
   Image1b disp8_1c;
   const float max_disp = 64.0f;
