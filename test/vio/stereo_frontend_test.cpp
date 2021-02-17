e13a4575efe619465880a358076c6d594ce93c9b
Milo Knowles <mknowles@mit.edu>
2021-02-17 17:05:10 -0500

    Test on V1_01_EASY also

--- test/vio/stereo_frontend_test.cpp
+++ test/vio/stereo_frontend_test.cpp
@@ -15,9 +15,8 @@ using namespace core;
 using namespace vio;
 
 
-TEST(VioTest, TestStereoFrontendSequence)
+TEST(VioTest, TestStereoFrontendFarmsim)
 {
-  // const std::string toplevel_folder = "/home/milo/datasets/euroc/V1_01_EASY";
   const std::string toplevel_folder = "/home/milo/datasets/Unity3D/farmsim/euroc_test1";
   dataset::EurocDataset dataset(toplevel_folder);
 
@@ -25,6 +24,34 @@ TEST(VioTest, TestStereoFrontendSequence)
 
   const PinholeCamera camera_model(415.876509, 415.876509, 375.5, 239.5, 480, 752);
   const StereoCamera stereo_rig(camera_model, 0.2);
+  StereoFrontend stereo_frontend(opt, stereo_rig);
+
+  cv::namedWindow("StereoTracking", cv::WINDOW_AUTOSIZE);
+
+  dataset::StereoCallback callback = [&](const StereoImage& stereo_data)
+  {
+    Matrix4d T_prev_cur_prior = Matrix4d::Identity();
+    const StereoFrontend::Result& result = stereo_frontend.Track(stereo_data, T_prev_cur_prior);
+    const Image3b viz = stereo_frontend.VisualizeFeatureTracks();
+    cv::imshow("StereoTracking", viz);
+    cv::waitKey(1);
+  };
+
+  dataset.RegisterStereoCallback(callback);
+  dataset.Playback(5.0f, false);
+  LOG(INFO) << "DONE" << std::endl;
+}
+
+
+TEST(VioTest, TestStereoFrontendEurocMav)
+{
+  const std::string toplevel_folder = "/home/milo/datasets/euroc/V1_01_EASY";
+  dataset::EurocDataset dataset(toplevel_folder);
+
+  StereoFrontend::Options opt;
+
+  const PinholeCamera camera_model(458.654, 457.296, 367.215, 248.375, 480, 752);
+  const StereoCamera stereo_rig(camera_model, 0.11);
   StereoFrontend stereo_frontend(opt, stereo_rig);
 
   cv::namedWindow("StereoTracking", cv::WINDOW_AUTOSIZE);
