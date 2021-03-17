fbb8a321f5fc9bdaa47ea0fbc2c4904863f33138
Milo Knowles <mknowles@mit.edu>
2021-03-17 16:13:39 -0400

    Single axis factor for depth

---
+++ src/vehicle/vio/single_axis_factor.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <gtsam/nonlinear/NonlinearFactor.h>
+#include <gtsam/geometry/Pose3.h>
+
+namespace gtsam {
+
+class SingleAxisFactor : public gtsam::NoiseModelFactor1<gtsam::Pose3> {
+ public:
+  enum Axis { X = 0, Y = 1, Z = 2 };
+
+  explicit SingleAxisFactor(gtsam::Key pose_key,
+                            Axis axis,
+                            double value,
+                            gtsam::SharedNoiseModel noise_model)
+      : gtsam::NoiseModelFactor1<gtsam::Pose3>(noise_model, pose_key),
+        axis_(axis),
+        value_(value)
+  {
+    // Precompute the Jacobian based on which axis corresponds to depth.
+    // Pose variables: [ rx ry rz tx ty tz ].
+    const int pose_axis_index = (3 + axis_);
+    J_(0, pose_axis_index) = 1.0;
+  }
+
+  // Returns the error and Jacobian of this factor at a linearization point.
+  gtsam::Vector evaluateError(const gtsam::Pose3& P_world_body,
+                              boost::optional<gtsam::Matrix&> J1 = boost::none) const
+  {
+    if (J1) { *J1 = J_; }
+    return gtsam::Vector1(P_world_body.translation()(axis_) - value_);
+  }
+
+ private:
+  Axis axis_;
+  double value_;
+  gtsam::Matrix16 J_ = gtsam::Matrix16::Zero();
+};
+
+}
---
+++ test/vio/single_axis_factor_test.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+
+#include "vio/gtsam_types.hpp"
+#include "vio/single_axis_factor.hpp"
+
+using namespace bm;
+using namespace vio;
+
+
+TEST(VioTest, TestSingleAxisFactor_01)
+{
+  const gtsam::Symbol pose1_sym('X', 0);
+  const IsotropicModel::shared_ptr noise_model = IsotropicModel::Sigma(1, 3.0);
+
+  const gtsam::SingleAxisFactor f(pose1_sym, gtsam::SingleAxisFactor::Axis::X, 123.456, noise_model);
+
+  const gtsam::Pose3 pose1 = gtsam::Pose3::identity();
+
+  gtsam::Matrix J;
+  const gtsam::Vector1& error = f.evaluateError(pose1, J);
+
+  std::cout << "J:\n" << J << std::endl;
+
+  gtsam::Matrix16 expected_J;
+  expected_J << 0, 0, 0, 1, 0, 0;
+
+  EXPECT_EQ(expected_J, J);
+  EXPECT_EQ(-123.456, error(0));
+}
