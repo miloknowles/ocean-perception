59f76f8232875fe19763ba2cdd3d3d81090d3524
Milo Knowles <mknowles@mit.edu>
2021-03-31 16:15:18 -0400

    Add some more lcmtypes

--- test/lcmtypes/test_publish.cpp
+++ test/lcmtypes/test_publish.cpp
@@ -4,8 +4,9 @@
 #include <lcm/lcm-cpp.hpp>
 
 // LCM types go into build/vehicle/lcmtypes, which we add as an include path.
-#include "vehicle/imu_t.hpp"
-#include "vehicle/range_t.hpp"
+#include "vehicle/imu_measurement_t.hpp"
+#include "vehicle/range_measurement_t.hpp"
+#include "vehicle/pose3_stamped_t.hpp"
 
 #include "core/eigen_types.hpp"
 
@@ -20,11 +21,13 @@ TEST(LcmtypesTest, TestPublish)
     LOG(FATAL) << "LCM failed" << std::endl;
   }
 
-  vehicle::imu_t imu_data;
-  imu_data.timestamp = 123;
-  imu_data.a[0] = 1.1;
-  imu_data.a[1] = 2.2;
-  imu_data.a[2] = -3.3;
+  vehicle::imu_measurement_t imu_msg;
+  imu_msg.header.timestamp = 123;
+  imu_msg.header.seq = 0;
+  imu_msg.header.frame_id = "imu0";
+  imu_msg.linear_acc.x = 1.1;
+  imu_msg.linear_acc.y = 1.2;
+  imu_msg.linear_acc.z = -1.3;
 
-  lcm.publish("example_channel", &imu_data);
+  lcm.publish("example_channel", &imu_msg);
 }
