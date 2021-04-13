644463236d872b4a7f7bdb9ef16dec198ed4221b
Milo Knowles <mknowles@mit.edu>
2021-04-13 11:38:44 -0400

    Centralized dataset params

--- src/sandbox/mesher_demo/main.cpp
+++ src/sandbox/mesher_demo/main.cpp
@@ -13,12 +13,14 @@
 #include "core/params_base.hpp"
 #include "core/pinhole_camera.hpp"
 #include "core/stereo_camera.hpp"
+#include "core/data_manager.hpp"
+#include "core/path_util.hpp"
+
 #include "dataset/data_provider.hpp"
 #include "dataset/euroc_dataset.hpp"
 #include "dataset/himb_dataset.hpp"
 #include "dataset/caddy_dataset.hpp"
 #include "dataset/acfr_dataset.hpp"
-#include "core/data_manager.hpp"
 #include "mesher/object_mesher.hpp"
 
 
@@ -29,10 +31,11 @@ using namespace mesher;
 
 enum Dataset
 {
-  EUROC = 0,
+  FARMSIM = 0,
   CADDY = 1,
   HIMB = 2,
-  ACFR = 3
+  ACFR = 3,
+  ZEDM = 4
 };
 
 
@@ -43,7 +46,7 @@ struct MesherDemoParams : public ParamsBase
 
   std::string folder;
   std::string subfolder;  // e.g "genova-A" for CADDY, "train" for HIMB
-  Dataset dataset = Dataset::EUROC;
+  Dataset dataset = Dataset::FARMSIM;
   float playback_speed = 4.0;
   bool pause = false;
   int input_height = 480.0;
@@ -66,58 +69,44 @@ struct MesherDemoParams : public ParamsBase
 
 int main(int argc, char const *argv[])
 {
-  MesherDemoParams params(Join("/home/milo/bluemeadow/catkin_ws/src/vehicle/src/sandbox/mesher_demo/config", "MesherDemo_params.yaml"));
+  MesherDemoParams params(sandbox_path("mesher_demo/config/MesherDemo_params.yaml"));
 
-  std::string shared_params_path = Join("/home/milo/bluemeadow/catkin_ws/src/vehicle/src/sandbox/mesher_demo/config", "shared_params.yaml");
+  std::string shared_params_path;
   dataset::DataProvider dataset;
 
   switch (params.dataset) {
-    case Dataset::EUROC:
+    case Dataset::FARMSIM:
       dataset = dataset::EurocDataset(params.folder);
+      shared_params_path = config_path("auv_base/Farmsim.yaml");
       break;
     case Dataset::CADDY:
+      throw std::runtime_error("No config available for CADDY");
       dataset = dataset::CaddyDataset(params.folder, params.subfolder);
       break;
     case Dataset::HIMB:
       dataset = dataset::HimbDataset(params.folder, params.subfolder);
+      shared_params_path = config_path("auv_base/HIMB.yaml");
       break;
     case Dataset::ACFR:
       dataset = dataset::AcfrDataset(params.folder);
-      shared_params_path = Join("/home/milo/bluemeadow/catkin_ws/src/vehicle/src/sandbox/mesher_demo/config", "acfr_params.yaml");
+      shared_params_path = config_path("auv_base/ACFR.yaml");
+      break;
+    case Dataset::ZEDM:
+      dataset = dataset::EurocDataset(params.folder);
+      shared_params_path = config_path("auv_base/ZEDMini.yaml");
       break;
     default:
       LOG(FATAL) << "Unknown dataset type: " << params.dataset << std::endl;
       break;
   }
 
-  // Make an (ordered) queue of all groundtruth poses.
-  // const std::vector<dataset::GroundtruthItem>& groundtruth_poses = dataset.GroundtruthPoses();
-  // CHECK(!groundtruth_poses.empty()) << "No groundtruth poses found" << std::endl;
-
-  // vio::DataManager<dataset::GroundtruthItem> gt_manager(groundtruth_poses.size(), true);
-  // for (const dataset::GroundtruthItem& gt : groundtruth_poses) {
-  //   gt_manager.Push(gt);
-  // }
-
-  ObjectMesher::Params mparams(
-      Join("/home/milo/bluemeadow/catkin_ws/src/vehicle/src/sandbox/mesher_demo/config", "ObjectMesher_params.yaml"),
+  ObjectMesher::Params mesher_params(
+      sandbox_path("mesher_demo/config/ObjectMesher_params.yaml"),
       shared_params_path);
-  ObjectMesher mesher(mparams);
+  ObjectMesher mesher(mesher_params);
 
   dataset::StereoCallback1b stereo_cb = [&](const StereoImage1b& stereo_pair)
   {
-    // const double time = ConvertToSeconds(stereo_pair.timestamp);
-
-    // Get the groundtruth pose nearest to this image.
-    // gt_manager.DiscardBefore(time);
-    // const dataset::GroundtruthItem gt = gt_manager.Pop();
-    // CHECK(std::fabs(ConvertToSeconds(gt.timestamp) - time) < 0.05) << "Timestamps not close enough" << std::endl;
-
-    // const Matrix4d world_T_cam = gt.world_T_body * params.body_T_cam;
-    // const Vector3d translation = world_T_cam.block<3, 1>(0, 3) - world_T_cam_prev.block<3, 1>(0, 3);
-    // Quaternionf q(world_T_cam.block<3, 3>(0, 0).cast<float>());
-    // Vector3f t(world_T_cam.block<3, 1>(0, 3).cast<float>());
-
     if (stereo_pair.left_image.rows > params.input_height) {
       StereoImage1b pair_downsized = StereoImage1b(stereo_pair.timestamp, stereo_pair.camera_id, Image1b(), Image1b());
       const double height = static_cast<double>(stereo_pair.left_image.rows);
