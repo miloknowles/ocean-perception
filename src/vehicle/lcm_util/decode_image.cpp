5f47cb685a73c18b3e4c96be336b4699bd07de80
Milo Knowles <mknowles@mit.edu>
2021-04-19 14:11:00 -0400

    Shared memory workinggit add -A!

--- src/vehicle/lcm_util/decode_image.cpp
+++ src/vehicle/lcm_util/decode_image.cpp
@@ -7,6 +7,7 @@
 
 namespace bm {
 
+
 void DecodeJPG(const vehicle::image_t& msg, cv::Mat& out)
 {
   CHECK_EQ("jpg", msg.encoding) << "Expected JPG image" << std::endl;
@@ -29,5 +30,30 @@ void DecodeJPG(const vehicle::image_t& msg, cv::Mat& out)
     cv::cvtColor(out, out, cv::COLOR_RGB2BGR);
   }
 }
+
+
+void DecodeJPG(const vehicle::mmf_image_t& msg, const uint8_t* buf_data, cv::Mat& out)
+{
+  CHECK_EQ("jpg", msg.encoding) << "Expected JPG image" << std::endl;
+
+  const bool is_color = msg.format == "rgb8" || msg.format == "bgr8";
+  const bool is_gray = msg.format == "mono8";
+  CHECK(is_color || is_gray) << "Unrecognized image format specifier: " << msg.format << std::endl;
+
+  const int buf_size = msg.size;
+  if (buf_size <= 0) {
+    LOG(WARNING) << "Tried to decode an image_t with size <= 0. Probably a mistake in the publisher." << std::endl;
+  }
+
+  // const uchar* buf_data = msg.data.data();
+  cv::Mat raw_data(1, buf_size, is_color ? CV_8UC3 : CV_8UC1, (void*)buf_data);
+  cv::imdecode(raw_data, is_color ? cv::IMREAD_COLOR : cv::IMREAD_GRAYSCALE, &out);
+
+  // Might need to flip channels to convert RBG -> BGR.
+  if (is_color && msg.format == "rgb8") {
+    cv::cvtColor(out, out, cv::COLOR_RGB2BGR);
+  }
+}
+
 
 }
--- src/vehicle/lcm_util/decode_image.hpp
+++ src/vehicle/lcm_util/decode_image.hpp
@@ -3,10 +3,14 @@
 #include <opencv2/core/types.hpp>
 
 #include "vehicle/image_t.hpp"
+#include "vehicle/mmf_image_t.hpp"
 
 namespace bm {
 
 // https://stackoverflow.com/questions/14727267/opencv-read-jpeg-image-from-buffer
 void DecodeJPG(const vehicle::image_t& msg, cv::Mat& out);
+
+// Decodes a JPG image from a buffer of uint8_t data.
+void DecodeJPG(const vehicle::mmf_image_t& msg, const uint8_t* buf_data, cv::Mat& out);
 
 }
