2c1f61fcd81f597a25294b7d114110fc4d575d4c
Milo Knowles <mknowles@mit.edu>
2021-05-18 10:48:44 -0400

    Bilinear interp working

--- src/sandbox/cuda_examples/get_subpixel.cu
+++ src/sandbox/cuda_examples/get_subpixel.cu
@@ -28,7 +28,7 @@ __device__ __forceinline__
 T GetSubpixel(const cu::PtrStepSz<T> im, float row, float col)
 {
   if (row < 0 || row >= im.rows || col < 0 || col >= im.cols) {
-    return;
+    return 0;
   }
 
   int row0 = __float2int_rd(row);
@@ -36,13 +36,13 @@ T GetSubpixel(const cu::PtrStepSz<T> im, float row, float col)
   int col0 = __float2int_rd(col);
   int col1 = min(__float2int_ru(col), im.cols - 1);
 
-  float c00 = GetPixel<float>(im, row0, col0);
-  float c01 = GetPixel<float>(im, row0, col1);
-  float c10 = GetPixel<float>(im, row1, col0);
-  float c11 = GetPixel<float>(im, row1, col1);
+  float c00 = im(row0, col0);
+  float c01 = im(row0, col1);
+  float c10 = im(row1, col0);
+  float c11 = im(row1, col1);
 
-  float trow = row - row0;
-  float tcol = col - col0;
+  float trow = row - __int2float_rn(row0);
+  float tcol = col - __int2float_rn(col0);
 
   float c0 = (1.0f - trow) * c00 + trow * c10;
   float c1 = (1.0f - trow) * c01 + trow * c11;
@@ -61,44 +61,38 @@ void GetRectSubpixel(const cu::PtrStepSz<float> im, float row0, float col0, int 
   if (tCol < width && tRow < height) {
     const float row = row0 + __int2float_rd(tRow);
     const float col = col0 + __int2float_rd(tCol);
-    const float interp = GetSubpixel<float>(im, row, col);
-    SetPixel<float>(out, tRow, tCol, interp);
+    out(tRow, tCol) = GetSubpixel<float>(im, row, col);
   }
 }
 
 
-int main (int argc, char* argv[])
+int main(int argc, char* argv[])
 {
   cv::Mat src_host = cv::imread("./resources/farmsim_01_left.png", CV_LOAD_IMAGE_GRAYSCALE);
   cv::cuda::GpuMat dst, src;
   src.upload(src_host);
 
   src.convertTo(dst, CV_32FC1);
+  // cv::cuda::GpuMat rect(cv::Size(300, 300), CV_32FC1);
 
-  cv::cuda::GpuMat rect(cv::Size(100, 100), CV_32FC1);
+  cv::cuda::GpuMat rect, rect1b;
+  rect.create(cv::Size(300, 300), CV_32FC1);
 
-  // Specify a reasonable block size
 	const dim3 block(16, 16);
-
-	// Calculate grid size to cover the whole image
-	// const dim3 grid((rect.cols + block.x - 1)/block.x, (rect.rows + block.y - 1)/block.y);
   const dim3 grid(cu::device::divUp(rect.cols, block.x), cu::device::divUp(rect.rows, block.y));
-
   GetRectSubpixel<<<grid, block>>>(dst, 53.4, 11.123, rect.rows, rect.cols, rect);
 
-  cv::cuda::threshold(src, dst, 128.0, 255.0, CV_THRESH_BINARY);
-
+  rect.convertTo(rect1b, CV_8UC1);
   cudaDeviceSynchronize();
 
-  cv::Mat result_host;
-  dst.download(result_host);
-
   cv::Mat rect_host;
-  rect.download(rect_host);
+  rect1b.download(rect_host);
 
-  cv::imshow("Result", result_host);
+  double minVal, maxVal;
+  cv::minMaxLoc(rect_host, &minVal, &maxVal);
+  printf("minVal=%f maxVal=%f\n", minVal, maxVal);
+
   cv::imshow("rect", rect_host);
-
   cv::waitKey();
 
   return 0;
